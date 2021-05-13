bafe7b27ac46cf0bf395570e0ff055600418396c
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-13 15:04:09 -0600

    Fix title of test

--- spec/features/franchisors/index_spec.rb
+++ spec/features/franchisors/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Franchisors' do
+RSpec.describe 'Franchisors name page' do
   before :each do
     @cfa = Franchisor.create!(name: "Chick-Fil-A", hq_city: "Atlanta", hq_state: "Georgia", quick_service: true, franchisee_cost: 10000.0)
     @applebees = Franchisor.create!(name: "Applebees", hq_city: "Glendale", hq_state: "California", quick_service: false, franchisee_cost: 30000.0)
@@ -11,5 +11,12 @@ RSpec.describe 'Franchisors' do
 
     expect(page).to have_content(@cfa.name)
     expect(page).to have_content(@applebees.name)
+  end
+
+  it 'shows all of the names of the franchisors' do
+    visit "/franchisors/1"
+
+    expect(page).to have_content(@cfa.name)
+    expect(page).to have_content(@cfa.quick_services)
   end
 end
