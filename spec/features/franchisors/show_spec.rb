74935b0ece3ed76356e23359fbba126473287945
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-14 22:52:29 -0600

    Create test for show of franchisees count of a franchisor

--- spec/features/franchisors/show_spec.rb
+++ spec/features/franchisors/show_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Franchisors show page' do
   before :each do
     @cfa = Franchisor.create!(name: "Chick-Fil-A", hq_city: "Atlanta", hq_state: "Georgia", quick_service: true, franchisee_cost: 10000.0)
+    @cfa_littleton = @cfa.franchisees.create!(name: "CFA Littleton", city: "Littleton", state: "Colorado", independent: false, annual_sales: 425000.0, initial_fee: 10000.0, pct_fee: 0.12)
+    @cfa_lakewood = @cfa.franchisees.create!(name: "CFA Lakewood", city: "Lakewood", state: "Colorado", independent: false, annual_sales: 437000.0, initial_fee: 10000.0, pct_fee: 0.13)
+    @cfa_tacoma = @cfa.franchisees.create!(name: "CFA Tacoma", city: "Tacoma", state: "Washington", independent: false, annual_sales: 451000.0, initial_fee: 10000.0, pct_fee: 0.15)
+
     @applebees = Franchisor.create!(name: "Applebees", hq_city: "Glendale", hq_state: "California", quick_service: false, franchisee_cost: 30000.0)
   end
 
@@ -18,5 +22,11 @@ RSpec.describe 'Franchisors show page' do
 
     expect(page).to have_content(@applebees.name)
     expect(page).to have_content(@applebees.franchisee_cost)
+  end
+
+  it 'shows the number of franchisees of the franchisor' do
+    visit "/franchisors/#{@cfa.id}"
+
+    expect(page).to have_content(@cfa.franchisees.count)
   end
 end
