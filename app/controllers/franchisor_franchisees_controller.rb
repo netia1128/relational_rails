c96bd0cc2de404bdedaec95fa7683675b202b942
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-15 16:20:50 -0600

    Fix minor issue with naming

--- app/controllers/franchisor_franchisees_controller.rb
+++ app/controllers/franchisor_franchisees_controller.rb
@@ -1,6 +1,6 @@
 class FranchisorFranchiseesController < ApplicationController
   def index
-    @franchisor = Franchisor.find(params[:franchisor_id])
+    @franchisor = Franchisor.find(params[:id])
     @franchisees = @franchisor.franchisees
   end
 end
