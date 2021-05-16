6e742f5e314679fb1fdbe2d8f6657d0dbb4ff4fb
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-16 14:12:02 -0600

    Create model test for order

--- spec/models/franchisor_spec.rb
+++ spec/models/franchisor_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Franchisor, type: :model do
   it {should have_many :franchisees}
+
+  describe 'instance methods' do
+    before :each do
+      @cfa = Franchisor.create!(name: "Chick-Fil-A", hq_city: "Atlanta", hq_state: "Georgia", quick_service: true, franchisee_cost: 10000.0)
+      @applebees = Franchisor.create!(name: "Applebees", hq_city: "Glendale", hq_state: "California", quick_service: false, franchisee_cost: 30000.0)
+    end
+
+    describe '#sort_by_created_desc' do
+      it 'sorts by the most recently created' do
+        expect(@applebees.created_at > @cfa.created_at).to eq(true)
+      end
+    end
+  #   describe '#count_franchisees' do
+  #     before :each do
+  #       @cfa = Franchisor.create!(name: "CFA", hq_city: "Atlanta", hq_state: "Georgia", quick_service: true, franchisee_cost: 10000.0)
+  #       @cfa_littleton = @cfa.franchisees.create!(name: "CFA Littleton", city: "Littleton", state: "Colorado", independent: false, annual_sales: 425000.0, initial_fee: 10000.0, pct_fee: 0.12)
+  #       @cfa_lakewood = @cfa.franchisees.create!(name: "CFA Lakewood", city: "Lakewood", state: "Colorado", independent: false, annual_sales: 437000.0, initial_fee: 10000.0, pct_fee: 0.13)
+  #       @cfa_tacoma = @cfa.franchisees.create!(name: "CFA Tacoma", city: "Tacoma", state: "Washington", independent: false, annual_sales: 451000.0, initial_fee: 10000.0, pct_fee: 0.15)
+  #     end
+  #
+  #     it 'counts the number of franchisees to this franchisor' do
+  #       expect(@cfa.franchisees.count).to eq(3)
+  #     end
+  #   end
+  end
 end
