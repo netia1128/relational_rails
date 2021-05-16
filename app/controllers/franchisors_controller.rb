3b557250bd9e1f689bf83b7e907ef3257206b884
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-16 12:15:18 -0600

    Implement new and create methods

--- app/controllers/franchisors_controller.rb
+++ app/controllers/franchisors_controller.rb
@@ -6,4 +6,18 @@ class FranchisorsController < ApplicationController
   def show
     @franchisor = Franchisor.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @franchisor = Franchisor.create(franchisor_params)
+    redirect_to "/franchisors"
+  end
+
+  private
+
+  def franchisor_params
+    params.permit(:name, :hq_city, :hq_state, :quick_service, :franchisee_cost)
+  end
 end
