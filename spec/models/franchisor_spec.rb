078611aa4800b3c28dd399ac09709a9f8e4fe811
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-16 14:21:30 -0600

    Refactor model test for desc order via created_at

--- spec/models/franchisor_spec.rb
+++ spec/models/franchisor_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Franchisor, type: :model do
 
     describe '#sort_by_created_desc' do
       it 'sorts by the most recently created' do
-        expect(@applebees.created_at > @cfa.created_at).to eq(true)
+        expect(Franchisor.sort_by_created_desc.first).to eq(@applebees)
       end
     end
   #   describe '#count_franchisees' do
