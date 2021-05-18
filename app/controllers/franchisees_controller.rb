ff0ef82577e6e4912a0102f5c4e38736b627b011
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-17 19:12:52 -0600

    Implement edit and update methods

--- app/controllers/franchisees_controller.rb
+++ app/controllers/franchisees_controller.rb
@@ -6,4 +6,20 @@ class FranchiseesController < ApplicationController
   def show
     @franchisee = Franchisee.find(params[:id])
   end
+
+  def edit
+    @franchisee = Franchisee.find(params[:id])
+  end
+
+  def update
+    @franchisee = Franchisee.find(params[:id])
+    @franchisee.update(franchisee_params)
+    redirect_to "/franchisees/#{@franchisee.id}"
+  end
+
+  private
+
+  def franchisee_params
+    params.permit(:name, :city, :state, :independent, :annual_sales, :initial_fee, :pct_fee)
+  end
 end
